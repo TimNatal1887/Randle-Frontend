b52ff25b42f34c0d220d756dba919b895df3e108
Timothy Natal <timothynatal@pursuit.org>
2024-04-18 13:47:48 -0400

    profanity filter added

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import "../Styles/Register.css"
+import BadWordsNext from 'bad-words-next';
+import en from 'bad-words-next/data/en.json'
+
+const badwords = new BadWordsNext({ data: en })
 
 const URL = import.meta.env.VITE_BASE_URL;
 
 const Register = ({ setToggleLogin }) => {
   const navigate = useNavigate();
   const [user, setUser] = useState({ username: "", password: "", email: "", total_score:0});
-
+  const [error, setError] = useState(false)
   function handleChange(event) {
     setUser({ ...user, [event.target.id]: event.target.value });
   }
@@ -15,32 +19,40 @@ const Register = ({ setToggleLogin }) => {
   async function handleSubmit(e) {
     e.preventDefault();
 
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+    const badUserNameCheck = badwords.check(user.username)
+    const badEmailCheck = badwords.check(user.email)
 
-      body: JSON.stringify(user),
-    };
-
-    try {
-      const res = await fetch(`${URL}/api/auth/register`, options);
-
-      if (!res.ok) throw new Error("Registration failed");
-      const data = await res.json();
-
-      if (data.token) {
-        // in case there is an old token in the browser, remove it
-        localStorage.removeItem("token");
-        // set the new user's JWT token in the browser
-        localStorage.setItem("token", data.token);
-        setToggleLogin(true);
-        navigate("/dashboard");
+    if(!badUserNameCheck && !badEmailCheck){
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+  
+        body: JSON.stringify(user),
+      };
+  
+      try {
+        const res = await fetch(`${URL}/api/auth/register`, options);
+  
+        if (!res.ok) throw new Error("Registration failed");
+        const data = await res.json();
+  
+        if (data.token) {
+          // in case there is an old token in the browser, remove it
+          localStorage.removeItem("token");
+          // set the new user's JWT token in the browser
+          localStorage.setItem("token", data.token);
+          setToggleLogin(true);
+          navigate("/dashboard");
+        }
+      } catch (error) {
+        console.error("Error during registration:", error);
       }
-    } catch (error) {
-      console.error("Error during registration:", error);
+    }else{
+      setError(true)
     }
+
   }
 
   // USE THIS FORM TO BUILD OUT YOUR FORM PROPERLY BY ADDING LABELS AND INPUTS AS WELL AS WHATEVER CSS FRAMEWORK YOU MAY USE OR VANILLA CSS. THIS IS JUST A BOILERPLATE CODE
@@ -90,6 +102,7 @@ const Register = ({ setToggleLogin }) => {
         </label>
 
         <button className="register-button">Submit</button>
+        {error && <p className="error"> Please enter a different username/password!</p>}
       </form>
       </div>
     </div>
